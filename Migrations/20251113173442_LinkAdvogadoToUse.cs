40d4f9819d1836b70c69c76df366cb81ae1a52b9
vitor-alves95 <152516551+vitor-alves95@users.noreply.github.com>
2025-11-13 14:35:46 -0300

    Nova migration na tabela advogado

---
+++ Migrations/20251113173442_LinkAdvogadoToUse.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscritorioAdvocacia.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkAdvogadoToUse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Advogados",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advogados_ApplicationUserId",
+                table: "Advogados",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Advogados_AspNetUsers_ApplicationUserId",
+                table: "Advogados",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Advogados_AspNetUsers_ApplicationUserId",
+                table: "Advogados");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Advogados_ApplicationUserId",
+                table: "Advogados");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Advogados");
+        }
+    }
+}
--- Models/Advogado.cs
+++ Models/Advogado.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 namespace EscritorioAdvocacia.Models { 
 
     public class Advogado { 
@@ -7,6 +8,11 @@ namespace EscritorioAdvocacia.Models {
     public string Nome { get; set; }
     public string OAB { get; set; }
     public string Email { get; set; }
+
+
+    [ForeignKey("ApplicationUser")]
+    public string? ApplicationUserId { get; set; }
+    public virtual ApplicationUser? ApplicationUser { get; set; }
 
     // Um advogado pode ser responsável por vários processos
     public virtual ICollection<Processo> Processos { get; set; }
